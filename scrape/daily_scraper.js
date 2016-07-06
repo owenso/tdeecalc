3885cbfc55ed9651ebf8e33719cd3e67b23eb2b3
Owens O'Brien <owensobrien@gmail.com>
2016-07-06 18:50:05 -0400

    data scrape successful

---
+++ scrape/daily_scraper.js
@@ -0,0 +1,17 @@
+var mfp = require('mfp');
+var fs = require('fs');
+var moment = require ('moment');
+
+var yesterday = moment().subtract(1, 'days')
+mfp.fetchSingleDate('snewo531', moment(yesterday).format('YYYY-MM-DD'), 'all', function(data){
+    if (data) {
+        console.log(data);
+    } else {
+        var error = moment().format('x') + ': Unable to scrape data for ' + yesterday;
+        fs.appendFile('/logs/errorLog.txt', error , function (err) {
+            if (err) {
+                console.log('cant log errors');
+            };
+        });
+    }
+});
