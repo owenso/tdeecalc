369f1640a5ba64c32ffd20081f85bd11a37629d1
Owens O'Brien <owensobrien@gmail.com>
2016-07-07 03:00:52 -0400

    got something going now

--- config/express.js
+++ config/express.js
@@ -1,20 +1,29 @@
 var express = require('express'),
     morgan = require('morgan'),
     compress = require('compression'),
-    bodyParser = require('body-parser';
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
+module.exports = function(){
 
-var app = express();
+    var app = express();
 
-if (process.env.NODE_ENV === 'local') {
-    console.log('Running in local mode.')
-    app.use(morgan('dev'));
-} else if (process.env.NODE_ENV === 'production') {
-   console.log("Running in production mode.");
-   app.use(compress());
+    if (process.env.NODE_ENV === 'local') {
+        console.log('Running in local mode.')
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production') {
+       console.log("Running in production mode.");
+       app.use(compress());
+    }
+
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    app.use(express.static('../public'));
+
+    app.get('/', function(req,res) {
+        res.sendFile("/public/index.html", {"root": process.cwd()})
+    })
+
+    return app;
 }
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-app.use(express.static('../public'));
