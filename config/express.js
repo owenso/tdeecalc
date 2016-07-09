ba7385f5b65e6295bc36911108c7ed83f8779571
Owens O'Brien <owensobrien@gmail.com>
2016-07-09 01:06:48 -0400

    next stuff

--- config/express.js
+++ config/express.js
@@ -12,7 +12,7 @@ module.exports = function(){
     var app = express();
 
     if (process.env.NODE_ENV === 'local') {
-        console.log('Running in local mode.')
+        console.log('Running in local mode.');
         app.use(morgan('dev'));
     } else if (process.env.NODE_ENV === 'production') {
        console.log("Running in production mode.");
@@ -21,12 +21,11 @@ module.exports = function(){
 
     app.use(bodyParser.json());
     app.use(methodOverride());
+    app.use('/', express.static('public'));
 
-    app.use(express.static('../public'));
-
-    app.get('/', function(req,res) {
-        res.sendFile("/public/index.html", {"root": process.cwd()})
-    })
+    // app.get('/', function(req,res) {
+    //     res.sendFile("/public/index.html", {"root": process.cwd()})
+    // })
 
     return app;
-}
+};
