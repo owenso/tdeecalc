14acfa858be433dba973d6576abb7f861915cd22
Owens O'Brien <owensobrien@gmail.com>
2016-07-25 11:54:35 -0400

    change daily scraper to use pool for scaling

--- scrape/daily_scraper.js
+++ scrape/daily_scraper.js
@@ -4,28 +4,24 @@ const fs = require('fs');
 const moment = require('moment');
 const pg = require('pg');
 const _ = require('underscore');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 const config = require('../config/config');
 const connectionTest = require('../config/mfpConnectionTest.js')();
 const crypto = require('crypto');
+const Pool = require('pg-pool');
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
 
 let yesterday = moment().subtract(1, 'days');
 
-let scrapeAndInsert = function(user) {
+let scrapeAndInsert = function(user, client, done) {
     mfp.fetchSingleDate(user.mfp_username, moment(yesterday).format('YYYY-MM-DD'), 'all', function(data){
         if (data) {
-            let clientTwo = new pg.Client(config.pgConnectionString);
-            console.log(config.pgConnectionString)
             let dataSet = _.values(data);
             data.date = moment(data.date, 'YYYY-MM-DD').format('MM/DD/YYYY');
             let dataDate = dataSet.pop();
             let idHash = crypto.createHash('md5').update(data.date + user.mfp_username).digest("hex");
-            console.log('in here')
-            console.log(clientTwo)
-            clientTwo.query('INSERT INTO nutrition (id,calories,carbs,fat,protein,cholesterol,sodium,sugar,fiber,date_entered,users_id) VALUES (\'' + idHash + '\',' + dataSet + ', \'' + dataDate + '\',\'' + user.id + '\') ON CONFLICT (id) DO UPDATE SET (id,calories,carbs,fat,protein,cholesterol,sodium,sugar,fiber,date_entered,users_id) = (\'' + idHash + '\',' + dataSet + ', \'' + dataDate + '\',\'' + user.id + '\')', function(err, result) {
-                console.log('something happened')
-                clientTwo.end();
+            client.query('INSERT INTO nutrition (id,calories,carbs,fat,protein,cholesterol,sodium,sugar,fiber,date_entered,users_id) VALUES (\'' + idHash + '\',' + dataSet + ', \'' + dataDate + '\',\'' + user.id + '\') ON CONFLICT (id) DO UPDATE SET (id,calories,carbs,fat,protein,cholesterol,sodium,sugar,fiber,date_entered,users_id) = (\'' + idHash + '\',' + dataSet + ', \'' + dataDate + '\',\'' + user.id + '\')', function(err, result) {
+                done();
                 if (err) {
                     console.log(err);
                 } else {
@@ -43,12 +39,21 @@ let scrapeAndInsert = function(user) {
     });
 };
 
-let client = new pg.Client(config.pgConnectionString);
-client.connect();
-let query = client.query('SELECT * FROM users WHERE mfp_username IS NOT NULL', function(err, user) {
-    client.end();
-    for (let i=0; i<user.rows.length;i++){
-        console.log(user.rows[i])
-        scrapeAndInsert(user.rows[i]);
-    }
+let usersClient = new pg.Client(config.pgConnectionString);
+usersClient.connect();
+let query = usersClient.query('SELECT * FROM users WHERE mfp_username IS NOT NULL', function(err, user) {
+    usersClient.end();
+    let pool = new Pool(config.pgPoolSettings);
+    pool.connect(function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        for (let i=0; i<user.rows.length;i++){
+            console.log('scraping user #'+ i)
+            scrapeAndInsert(user.rows[i], client, done);
+        }
+    })
+    pool.on('error', function (err, client) {
+        console.error('idle client error', err.message, err.stack)
+    })
 });
